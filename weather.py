a2adf3e9008bdfc94ead43f7f5e955904d9a6ddc
sshlimovich <sshlimovich@panaya.com>
2021-02-26 23:24:51 +0200

    true weather script

--- weather.py
+++ weather.py
@@ -1,71 +1,33 @@
-# Python program to find current
-# weather details of any city
-# using openweathermap api
-
-# import required modules
-import requests, json
-
-# Enter your API key here
-api_key = "Your_API_Key"
-
-# base_url variable to store url
+import requests, json 
+  
+# Enter your API key here 
+api_key = "d71f373e048f9c80a4cd78ee5f5aa13e"
+#open weather map URL
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
+# relevant city
+city_name = "Tel-Aviv"
+# request URL
+query_url = base_url + "appid=" + api_key + "&q=" + city_name+ "&units=metric"
+#get the data
+response = requests.get(query_url)
+general_data = response.json()
 
-# Give city name
-city_name = input("Enter city name : ")
+#simple check if the pull worked and no connectivity issues
+if str(general_data["cod"]) == '200':
 
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
-
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
-
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-if x["cod"] != "404":
-
-    # store the value of "main"
-    # key in variable y
-    y = x["main"]
-
-    # store the value corresponding
-    # to the "temp" key of y
-    current_temperature = y["temp"]
-
-    # store the value corresponding
-    # to the "pressure" key of y
-    current_pressure = y["pressure"]
-
-    # store the value corresponding
-    # to the "humidity" key of y
-    current_humidiy = y["humidity"]
-
-    # store the value of "weather"
-    # key in variable z
-    z = x["weather"]
-
-    # store the value corresponding
-    # to the "description" key at
-    # the 0th index of z
-    weather_description = z[0]["description"]
+    main_data = general_data["main"]
+    #temperature
+    temperature = main_data["temp"]
+    # what it feels like 
+    Feels = main_data["feels_like"]
+    #get the description
+    weather = general_data["weather"]
+    weather_description = weather[0]["description"]
+  
     # print following values
-    print(" Temperature (in kelvin unit) = " +
-                    str(current_temperature) +
-          "\n atmospheric pressure (in hPa unit) = " +
-                    str(current_pressure) +
-          "\n humidity (in percentage) = " +
-                    str(current_humidiy) +
-          "\n description = " +
-                    str(weather_description))
-
+    print(" Temperature = " + str(temperature) +
+        "\n Description = " + str(weather_description) +
+        "\n Feels like = " + str(Feels)
+    )
 else:
-    print(" City Not Found ")
+    print("Error in request")
